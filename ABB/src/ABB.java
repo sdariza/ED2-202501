0ee28e97df58095df0f10ae6a5d761787b30d245
Sebastian David Ariza Coll <39573068+sdariza@users.noreply.github.com>
2025-02-13 19:52:59 -0500

    refactor: ♻️ remove root validation in addNodeABBR

--- ABB/src/ABB.java
+++ ABB/src/ABB.java
@@ -2,25 +2,21 @@ public class ABB {
     Node root;
 
     public void addNodeABBR(Node root, Node newNode) {
-        if (root == null) {
-            root = newNode;
+        if (root.data > newNode.data) {
+            if (root.left == null) {
+                root.left = newNode;
+            } else {
+                addNodeABBR(root.left, newNode);
+            }
         } else {
-            if (root.data > newNode.data) {
-                if (root.left == null) {
-                    root.left = newNode;
+            if (root.data < newNode.data) {
+                if (root.right == null) {
+                    root.right = newNode;
                 } else {
-                    addNodeABBR(root.left, newNode);
+                    addNodeABBR(root.right, newNode);
                 }
             } else {
-                if (root.data < newNode.data) {
-                    if (root.right == null) {
-                        root.right = newNode;
-                    } else {
-                        addNodeABBR(root.right, newNode);
-                    }
-                } else {
-                    System.out.printf("El elemento %d ya existe \n", newNode.data);
-                }
+                System.out.printf("El elemento %d ya existe \n", newNode.data);
             }
         }
     }
