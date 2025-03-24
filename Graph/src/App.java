70e6dc210d82ff4d5ba201d671540706d4af7a30
sdariza <sdariza@uninorte.edu.co>
2025-03-24 13:54:08 -0500

    feat: ✨ update Graph implementation with DFS and BFS methods, remove Edge class, and enhance App for graph operations

--- Graph/src/App.java
+++ Graph/src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Graph G = new Graph();
+        String[] nodes = { "1", "2", "3", "4", "5", "6", "7" };
+
+        for (String node : nodes) {
+            G.addNode(node);
+        }
+
+        G.addEdge("1", "2");
+        G.addEdge("1", "3");
+        G.addEdge("1", "4");
+
+        G.addEdge("2", "5");
+        G.addEdge("2", "6");
+
+        G.addEdge("3", "4");
+
+        G.addEdge("4", "5");
+        G.addEdge("4", "7");
+
+        G.addEdge("5", "6");
+        G.addEdge("5", "7");
+
+        G.addEdge("6", "7");
+
+        G.depthFirstSearch("2");
+
+        G.bfs("2");
     }
 }
--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class Graph {
     private Map<String, Node> nodes;
-    private List<Edge> edges;
     private Map<Node, List<Node>> adjacencyList;
 
     public Graph() {
         this.nodes = new HashMap<>();
-        this.edges = new ArrayList<>();
         this.adjacencyList = new HashMap<>();
     }
 
@@ -28,9 +28,60 @@ public class Graph {
         if (nodes.containsKey(vi) && nodes.containsKey(vj)) {
             Node nodeVi = nodes.get(vi);
             Node nodeVj = nodes.get(vj);
-            edges.add(new Edge(vi, vj));
             adjacencyList.get(nodeVi).add(nodeVj);
             adjacencyList.get(nodeVj).add(nodeVi);
         }
     }
+
+    public void showNodes() {
+        for (String node : nodes.keySet()) {
+            System.out.print(node + "|");
+        }
+        System.out.println();
+    }
+
+    public void depthFirstSearch(String startValue) {
+        System.out.println("DFS from: " + startValue);
+        Node startNode = nodes.get(startValue);
+        if (startNode != null) {
+            Map<Node, Boolean> visited = new HashMap<>();
+            dfs(startNode, visited);
+        }
+        System.out.println();
+    }
+
+    public void dfs(Node current, Map<Node, Boolean> visited) {
+        visited.put(current, true);
+        System.out.print("-->" + current.getValue());
+        for (Node neighbor : adjacencyList.get(current)) {
+            if (!visited.getOrDefault(neighbor, false)) {
+                dfs(neighbor, visited);
+            }
+        }
+    }
+
+    public void bfs(String startValue) {
+        Node startNode = nodes.get(startValue);
+        if (startNode == null)
+            return;
+
+        Map<Node, Boolean> visited = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        visited.put(startNode, true);
+        queue.add(startNode);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.print("-->" + current.getValue());
+
+            for (Node neighbor : adjacencyList.get(current)) {
+                if (!visited.getOrDefault(neighbor, false)) {
+                    visited.put(neighbor, true);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
 }
